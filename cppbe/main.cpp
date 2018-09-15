cf3033f54648a42235a9924d59060cc883072e48
Chen Zhou <uint0@users.noreply.github.com>
2018-09-15 13:51:38 -0400

    Added basic timing shit

---
+++ cppbe/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <libfreenect2/libfreenect2.hpp>
+#include <libfreenect2/frame_listener_impl.h>
+#include <libfreenect2/registration.h>
+#include <libfreenect2/logger.h>
+#include <chrono>
+
+#define ever (;;)
+
+int main() {
+    libfreenect2::Freenect2 freenect2;
+    libfreenect2::Freenect2Device *dev = 0;
+    libfreenect2::PacketPipeline *pipeline = new libfreenect2::CpuPacketPipeline();
+
+    if(freenect2.enumerateDevices() == 0) {
+        std::cout << "no device connected!" << std::endl;
+        return -1;
+    }
+
+    std::string serial = freenect2.getDefaultDeviceSerialNumber();
+
+    dev = freenect2.openDevice(serial, pipeline);
+
+    unsigned int types = libfreenect2::Frame::Color;
+
+    libfreenect2::SyncMultiFrameListener listener(types);
+    libfreenect2::FrameMap frames;
+    dev->setColorFrameListener(&listener);
+    dev->setIrAndDepthFrameListener(&listener);
+
+    dev->startStreams(true, false);
+
+
+    std::cout << "device serial: " << dev->getSerialNumber() << std::endl;
+    std::cout << "device firmware: " << dev->getFirmwareVersion() << std::endl;
+
+    auto time = std::chrono::high_resolution_clock().now();
+
+    for ever {
+        listener.waitForNewFrame(frames, 10 * 1000);
+        libfreenect2::Frame *rgb   = frames[libfreenect2::Frame::Color];
+
+        auto td = time - (time = std::chrono::high_resolution_clock().now());
+        std::cout << std::chrono::duration_cast<std::chrono::microseconds>(td).count() << std::endl;
+    }
+}
